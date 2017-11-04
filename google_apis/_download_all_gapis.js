c18479ecaccbf5b3ac00314296e78c579895609a
Robert Lancer <rob@collaborizm.com>
2017-11-03 23:40:45 -0400

    scripts to donwload all modules

--- google_apis/_download_all_gapis.js
+++ google_apis/_download_all_gapis.js
@@ -1,12 +1,17 @@
 const axios = require('axios')
 const fs = require('fs')
 
+const idToFilename = id => `${id.replace(":", "-").replace('.', '~')}.js`
 const downloadAllAPIs = async () => {
 
   try {
     const {data} = await axios({
       url: 'https://www.googleapis.com/discovery/v1/apis'
     })
+
+    const wsList = fs.createWriteStream(`_api_list.js`)
+    wsList.write(`module.exports = ${JSON.stringify(data)};`)
+    wsList.end()
 
     data.items.forEach(async item => {
 
@@ -17,12 +22,12 @@ const downloadAllAPIs = async () => {
       })
 
 
-      const ws = fs.createWriteStream(`${id.replace(":", "-").replace('.', '~')}.js`)
+      const ws = fs.createWriteStream(idToFilename(id))
       ws.write(`module.exports = ${JSON.stringify(itemData)};`)
       ws.end()
     })
 
-    console.log('Available APIs' , data.items.map(({name, id, version})=>`|${name} | ${version} | ${id.replace(":", "-").replace('.', '~')} | `).join('\n'))
+    console.log('Available APIs', data.items.map(({name, id, version}) => `|${name} | ${version} | ${idToFilename(id)} | `).join('\n'))
 
 
   }
