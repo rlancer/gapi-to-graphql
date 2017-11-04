0ab348cd78b85d6ba0b0206b122c9260c304cbaf
Robert Lancer <rob@collaborizm.com>
2017-11-04 00:52:58 -0400

    GoogleAPI list

--- google_apis/_download_all_gapis.js
+++ google_apis/_download_all_gapis.js
@@ -2,9 +2,11 @@ const axios = require('axios')
 const fs = require('fs')
 
 const idToFilename = id => `${id.replace(":", "-").replace('.', '~')}.js`
+
 const downloadAllAPIs = async () => {
 
   try {
+
     const {data} = await axios({
       url: 'https://www.googleapis.com/discovery/v1/apis'
     })
@@ -21,22 +23,17 @@ const downloadAllAPIs = async () => {
         url: discoveryRestUrl
       })
 
-
       const ws = fs.createWriteStream(idToFilename(id))
       ws.write(`module.exports = ${JSON.stringify(itemData)};`)
       ws.end()
     })
 
-    console.log('Available APIs', data.items.map(({name, id, version}) => `|${name} | ${version} | ${idToFilename(id)} | `).join('\n'))
-
-
+    console.log('Available APIs', data.items.map(({name, id, version}) => `|${name} | ${version} | ${idToFilename(id)} | require('gapi-to-graphql/google_apis/${idToFilename(id)}') |`).join('\n'))
   }
   catch (err) {
     console.error(err)
     throw err.response.data.error.message
   }
-
-
 }
 
 
