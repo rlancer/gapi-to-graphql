8c83fcb13d30fe2e9832167c7a430074b6ab9594
Robert Lancer <rob@collaborizm.com>
2017-11-06 00:56:06 -0500

    script to detect mal functinoing apis;

--- google_apis/_download_all_gapis.js
+++ google_apis/_download_all_gapis.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
 const fs = require('fs')
-
+const gapiToGraphL = require('../dist/index')
 const idToFilename = id => `${id.replace(":", "-").replace('.', '~')}.js`
+const gql = require('graphql')
 
 const downloadAllAPIs = async () => {
 
@@ -11,23 +12,36 @@ const downloadAllAPIs = async () => {
       url: 'https://www.googleapis.com/discovery/v1/apis'
     })
 
+    const sucsesfulApis = []
 
     data.items.forEach(async item => {
-
       const {name, id, version, discoveryRestUrl} = item
 
-      const {data: itemData} = await axios({
-        url: discoveryRestUrl
-      })
+      try {
 
-      const ws = fs.createWriteStream(idToFilename(id))
-      ws.write(`module.exports = ${JSON.stringify(itemData)};`)
-      ws.end()
+        const {data: itemData} = await axios({
+          url: discoveryRestUrl
+        })
+
+        gapiToGraphL({
+          gapiAsJsonSchema: itemData,
+          graphQLModule: gql
+        })
+
+        const ws = fs.createWriteStream(idToFilename(id))
+        ws.write(`module.exports = ${JSON.stringify(itemData)};`)
+        ws.end()
+
+        sucsesfulApis.push(item)
+
+      } catch (err) {
+        console.warn('Error processing ', id, err)
+      }
     })
 
 
     const wsList = fs.createWriteStream(`_api_list.js`)
-    wsList.write(`module.exports = ${JSON.stringify(data.items.map(({name, id, version}) => ({
+    wsList.write(`module.exports = ${JSON.stringify(sucsesfulApis.map(({name, id, version}) => ({
       module: idToFilename(id),
       name,
       id
@@ -35,7 +49,7 @@ const downloadAllAPIs = async () => {
     wsList.end()
 
 
-    console.log('Available APIs', data.items.map(({name, id, version}) => `|${name} | ${version} | require('gapi-to-graphql/google_apis/${idToFilename(id)}') |`).join('\n'))
+    console.log('Available APIs', sucsesfulApis.map(({name, id, version}) => `|${name} | ${version} | require('gapi-to-graphql/google_apis/${idToFilename(id)}') |`).join('\n'))
   }
   catch (err) {
     console.error(err)
