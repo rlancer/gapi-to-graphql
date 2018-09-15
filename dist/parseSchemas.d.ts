0e25960937c2615a905fece465d759f36e31d667
Robert Lancer <rob@collaborizm.com>
2018-09-14 22:33:54 -0400

    removed dist directory from node modules

--- dist/parseSchemas.d.ts
+++
@@ -1,9 +0,0 @@
-interface ISchema {
-    id: any;
-    type: any;
-    properties: any;
-    description: any;
-}
-declare const _default: (schemas: Map<string, ISchema>, graphQLModule: any) => {};
-export default _default;
-//# sourceMappingURL=parseSchemas.d.ts.map
--- dist/utils.d.ts
+++
@@ -1,6 +0,0 @@
-declare const upperFirst: (str: string) => string;
-declare const keyMap: (literal: any, valueOperator: any, keyTransform?: any) => {} | null;
-declare const keys: (obj: any) => string[];
-declare const values: (obj: any) => {}[];
-export { upperFirst, keyMap, keys, values };
-//# sourceMappingURL=utils.d.ts.map
