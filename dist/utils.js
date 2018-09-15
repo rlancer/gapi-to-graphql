59a4e17f69809cc4020a0824ac504d1e9f506447
Robert Lancer <rob@collaborizm.com>
2018-09-14 22:35:28 -0400

    removed .idea directory

---
+++ dist/utils.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var upperFirst = function (str) { return "" + str[0].toUpperCase() + str.substring(1); };
+exports.upperFirst = upperFirst;
+var keyMap = function (literal, valueOperator, keyTransform) {
+    if (!literal)
+        return null;
+    var r = {};
+    var index = 0;
+    keys(literal).forEach(function (key) {
+        var value = valueOperator(key, literal[key], index++);
+        if (value !== null)
+            r[keyTransform ? keyTransform(key) : key] = value;
+    });
+    return r;
+};
+exports.keyMap = keyMap;
+var keys = function (obj) {
+    return Object.keys(obj || {});
+};
+exports.keys = keys;
+var values = function (obj) {
+    return Object.values(obj || {});
+};
+exports.values = values;
+//# sourceMappingURL=utils.js.map
